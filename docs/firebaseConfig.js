63aba7cee6e382dc815d3ee2fff34eef29f90d65
Didii33 <97118898+Didii33@users.noreply.github.com>
2025-02-13 19:08:32 +0100

    v1.5

--- docs/firebaseConfig.js
+++ docs/firebaseConfig.js
@@ -1,11 +1,11 @@
 /BonsaiApp/docs/src/firebaseConfig.js
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyD8AQIlrh-wK1ken2YNPPupycc47-UJPig",
+    authDomain: "bonsaiapp-cfe83.firebaseapp.com",
+    projectId: "bonsaiapp-cfe83",
+    storageBucket: "bonsaiapp-cfe83.firebasestorage.app",
+    messagingSenderId: "596359983425",
+    appId: "1:596359983425:web:c4fc04b1e33ab7f2e6f1c0"
+  };
 
 export default firebaseConfig;
