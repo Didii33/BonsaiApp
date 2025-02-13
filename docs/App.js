55b75cd4314d019431e591023fc09b6176942877
Didii33 <97118898+Didii33@users.noreply.github.com>
2025-02-13 19:23:27 +0100

    v2.5

--- docs/App.js
+++ docs/App.js
@@ -6,7 +6,10 @@ const db = firebase.firestore();
 document.addEventListener('DOMContentLoaded', () => {
   const loginContainer = document.getElementById('login-container');
   const mainContainer = document.getElementById('main-container');
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
   const loginButton = document.getElementById('login-button');
+  const registerButton = document.getElementById('register-button');
   const logoutButton = document.getElementById('logout-button');
   const bonsaiList = document.getElementById('bonsai-list');
   const bonsaiInput = document.getElementById('bonsai-input');
@@ -28,13 +31,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Login-Funktion
   loginButton.addEventListener('click', () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    auth.signInWithEmailAndPassword(email, password)
       .then(result => {
         console.log('User signed in:', result.user);
       })
       .catch(error => {
         console.error('Error signing in:', error);
+      });
+  });
+
+  // Registrierungsfunktion
+  registerButton.addEventListener('click', () => {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    auth.createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        console.log('User registered:', result.user);
+      })
+      .catch(error => {
+        console.error('Error registering:', error);
       });
   });
 
