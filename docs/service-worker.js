20489e0afaa8a54920be33f063d845aadd74811d
Didii33 <97118898+Didii33@users.noreply.github.com>
2025-02-13 20:10:50 +0100

    v5.1

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,18 +1,28 @@
-self.addEventListener('install', (event) => {
+navigator.serviceWorker.register('/docs/service-worker.js')
+  .then(registration => {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  })
+  .catch(err => {
+    console.log('ServiceWorker registration failed: ', err);
+  });
+
+
+  self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open('bonsai-cache-v2').then((cache) => {  // Verwende eine neue Cache-Version
+      caches.open('bonsai-cache').then((cache) => {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/app.js',
-          '/firebaseConfig.js',
-          '/manifest.json',
-          '/favicon.ico',
+          '/docs/',
+          '/docs/index.html',
+          '/docs/style.css',
+          '/docs/app.js',
+          '/docs/firebaseConfig.js',
+          '/docs/manifest.json',
+          '/docs/favicon.ico',
         ]);
       })
     );
-  });
+});
+
   
   self.addEventListener('activate', (event) => {
     const cacheWhitelist = ['bonsai-cache-v2'];  // Definiere hier die neuen Cache-Namen
