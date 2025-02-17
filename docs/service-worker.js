42da92385c79865a17931790d9f4a682e4d05968
Didii33 <nici.bh@bluewin.ch>
2025-02-17 21:03:21 +0100

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -81,3 +81,17 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+
+// Service Worker Cache leeren
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
